21f2addf6a94c7f49b0620620f8d1a527aa70173
Giorgi Tevdoradze <tevdoradzege@gmail.com>
2023-08-11 22:39:27 +0400

    Adds board matrix to UI interactions

--- automata.cs
+++ automata.cs
@@ -4,10 +4,9 @@ using Cairo;
 using Color = Cairo.Color;
 using static Gdk.EventMask;
 
-
 // Class used to store the states of the points as integers (0, 1 or 2)
 class SimBoard {
-    int[,]? numBoard = null;
+    public int[,] numBoard;
 
     // Initialization with custom size, sets all states to 0
     public SimBoard (int size){
@@ -26,14 +25,16 @@ class DrawBoard : DrawingArea {
     Color green = new Color(0, 1, 0),
     yellow = new Color (1, 1, 0),
     black = new Color(0, 0, 0);
+    SimBoard numMatrix;
 
     double xCord;
     double yCord;
 
     ImageSurface board;
 
-    public DrawBoard() {
+    public DrawBoard(SimBoard inp) {
         board = new ImageSurface(Format.Rgb24, 500, 500);
+        numMatrix = inp;
 
         using (Context c = new Context(board)) {
             c.SetSourceColor(green);
@@ -54,21 +55,42 @@ class DrawBoard : DrawingArea {
         xCord = e.X;
         yCord = e.Y;
         
-        Context c = new Context(board);
-        fillCube(c, xCord, yCord);
+        int xIndex = ((int) xCord) / 50;
+        int yIndex = ((int) yCord) / 50;
+
+        numMatrix.numBoard[xIndex, yIndex] = 2;
+        fillBoard(numMatrix);
 
         QueueDraw();
         return true;
     }
 
-    void fillCube(Context c, double inpX, double inpY) {
-        int drawX  = ((int) xCord) / 50 * 50;
-        int drawY = ((int) yCord) / 50 * 50;
+    void fillCube(Context c, int inpX, int inpY, Color color) {
+        int drawX  = inpX * 50;
+        int drawY = inpY * 50;
 
-        c.SetSourceColor(yellow);
+        c.SetSourceColor(color);
         c.LineWidth = 1;
         c.Rectangle(x: drawX, y: drawY, width: 50, height: 50);
         c.Fill();
+    }
+
+    public void fillBoard(SimBoard InpBoard){
+        Context c = new Context(board);
+        int[,] matrix = InpBoard.numBoard;
+        int n = matrix.GetLength(0);
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if (matrix[i, j] == 0){
+                    fillCube(c, i, j, green);
+                } else if (matrix[i, j] == 1){
+                    fillCube(c, i, j, yellow);
+                } else{
+                    fillCube(c, i, j, black);
+                }
+            }
+        }
     }
 
 }
@@ -76,15 +98,21 @@ class DrawBoard : DrawingArea {
 
 // Gtk class of the board window UI
 class BoardWindow : Gtk.Window {
-    public BoardWindow() : base("3-State Automata") {
+    DrawBoard mainBoard;
+
+    public BoardWindow(SimBoard nums) : base("3-State Automata") {
         Resize(500, 500);
-        DrawBoard mainBoard = new DrawBoard();
+        mainBoard = new DrawBoard(nums);
         Add(mainBoard);
     }
 
     protected override bool OnDeleteEvent(Event e) {
         Application.Quit();
         return true;
+    }
+
+    public void Draw(SimBoard board){
+        mainBoard.fillBoard(board);
     }
 }
 
@@ -108,7 +136,9 @@ class MainLoop {
     public static void Main(string[] args){
         Application.Init();
 
-        BoardWindow main = new BoardWindow();
+        SimBoard board = new SimBoard(10);
+        BoardWindow main = new BoardWindow(board);
+        main.Draw(board);
         // ControlWindow control = new ControlWindow();
 
         // control.ShowAll();
