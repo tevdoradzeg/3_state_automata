6ce96ed643e160571978edc93eb34b1301e76084
Giorgi Tevdoradze <tevdoradzege@gmail.com>
2023-08-17 22:24:11 +0400

    Adds the rules file and optimizes code for easier rule input

--- automata.cs
+++ automata.cs
@@ -1,17 +1,19 @@
-﻿using Gdk;
+﻿using System.Collections.Generic;
+using Gdk;
 using Gtk;
 using Cairo;
 using Color = Cairo.Color;
 using static Gdk.EventMask;
 using Timeout = GLib.Timeout;
 
-enum Colors {Green, Yellow, Black};
 
+enum Colors {Green, Yellow, Black};
 
 // Class used to store the states of the points as integers (0, 1 or 2)
 class SimBoard {
     public int[,] numBoard;
     int n;
+    Rules rules;
 
     // Initialization with custom size, sets all states to 0
     public SimBoard (int size){
@@ -22,56 +24,74 @@ class SimBoard {
             }
         }
         n = size;
+        rules = new Rules();
     }
 
-    public void ApplyModRule() {
+    public void ApplyForestRule(){
+        ApplyRule(rules.forestRuleH, rules.forestRuleV);
+    }
+
+    public void ApplyRule(Dictionary<(int, int, int), int> ruleH, Dictionary<(int, int, int), int> ruleV){
         int[,] newBoardH = new int[n,n];
 
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
-                newBoardH[i,j] = ModHorizontal(i, j);
+                int left = ((j - 1) < 0) ? (n - 1) : (j - 1);
+                int right = ((j + 1) >= n) ? 0 : (j + 1);
+
+                newBoardH[i, j] = ruleH[(numBoard[i, left], numBoard[i, j], numBoard[i, right])];
             }
         }
-        
+
         numBoard = newBoardH;
         int[,] newBoardV = new int[n,n];
 
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
-                newBoardV[i,j] = ModVertical(i, j);
+                int up = ((i - 1) < 0) ? (n - 1) : (i - 1);
+                int down = ((i + 1) >= n) ? 0 : (i + 1);
+
+                newBoardV[i, j] = ruleV[(numBoard[up, j], numBoard[i, j], numBoard[down, j])];
             }
         }
 
         numBoard = newBoardV;
     }
 
-    int ModHorizontal(int i, int j){
-        int left = j - 1;
-        if (left < 0){
-            left = n - 1;
-        }
+    // public void ApplyModRule() {
+    //     int[,] newBoardH = new int[n,n];
 
-        int right = j + 1;
-        if (right >= n){
-            right = 0;
-        }
+    //     for (int i = 0; i < n; i++){
+    //         for (int j = 0; j < n; j++){
+    //             newBoardH[i,j] = ModHorizontal(i, j);
+    //         }
+    //     }
+        
+    //     numBoard = newBoardH;
+    //     int[,] newBoardV = new int[n,n];
 
-        return (numBoard[i, j] + numBoard[i, left] + numBoard[i, right]) % 3;
-    }
+    //     for (int i = 0; i < n; i++){
+    //         for (int j = 0; j < n; j++){
+    //             newBoardV[i,j] = ModVertical(i, j);
+    //         }
+    //     }
 
-    int ModVertical(int i, int j){
-        int up = i - 1;
-        if (up < 0){
-            up = n - 1;
-        }
+    //     numBoard = newBoardV;
+    // }
 
-        int down = i + 1;
-        if (down >= n){
-            down = 0;
-        }
+    // int ModHorizontal(int i, int j){
+    //     int left = ((j - 1) < 0) ? (n - 1) : (j - 1);
+    //     int right = ((j + 1) >= n) ? 0 : (j + 1);
 
-        return (numBoard[i, j] + numBoard[down, j] + numBoard[up, j]) % 3;
-    }
+    //     return (numBoard[i, j] + numBoard[i, left] + numBoard[i, right]) % 3;
+    // }
+
+    // int ModVertical(int i, int j){
+    //     int up = ((i - 1) < 0) ? (n - 1) : (i - 1);
+    //     int down = ((i + 1) >= n) ? 0 : (i + 1);
+
+    //     return (numBoard[i, j] + numBoard[down, j] + numBoard[up, j]) % 3;
+    // }
 
 }
 
@@ -89,7 +109,7 @@ class DrawBoard : DrawingArea {
     ImageSurface board;
 
     public DrawBoard(SimBoard inp) {
-        board = new ImageSurface(Format.Rgb24, 500, 500);
+        board = new ImageSurface(Format.Rgb24, 750, 750);
         numMatrix = inp;
         currColor = Colors.Green;
 
@@ -111,12 +131,12 @@ class DrawBoard : DrawingArea {
     protected override bool OnButtonPressEvent(EventButton e) {
         xCord = e.X;
         yCord = e.Y;
-        if (xCord > 500 || yCord > 500){
+        if (xCord > 750 || yCord > 750){
             return true;
         }
         
-        int xIndex = ((int) xCord) / 25;
-        int yIndex = ((int) yCord) / 25;
+        int xIndex = ((int) xCord) / 15;
+        int yIndex = ((int) yCord) / 15;
 
         int fillInt;
         if (currColor == Colors.Green){
@@ -136,12 +156,12 @@ class DrawBoard : DrawingArea {
     }
 
     void fillCube(Context c, int inpX, int inpY, Color color) {
-        int drawX  = inpX * 25;
-        int drawY = inpY * 25;
+        int drawX  = inpX * 15;
+        int drawY = inpY * 15;
 
         c.SetSourceColor(color);
         c.LineWidth = 1;
-        c.Rectangle(x: drawX, y: drawY, width: 25, height: 25);
+        c.Rectangle(x: drawX, y: drawY, width: 15, height: 15);
         c.Fill();
     }
 
@@ -168,7 +188,7 @@ class DrawBoard : DrawingArea {
     }
 
     public void ApplyStep(){
-        numMatrix.ApplyModRule();
+        numMatrix.ApplyForestRule();
         fillBoard(numMatrix);
         QueueDraw();
     }
@@ -181,7 +201,7 @@ class BoardWindow : Gtk.Window {
     bool playing = false;
 
     public BoardWindow(SimBoard nums) : base("3-State Automata") {
-        Resize(700, 500);
+        Resize(950, 750);
         mainBoard = new DrawBoard(nums);
         Box mainBox = new Box(Orientation.Horizontal, 1);
         Box rightSide = new Box(Orientation.Vertical, 5);
@@ -204,11 +224,23 @@ class BoardWindow : Gtk.Window {
         appplyStep.Clicked += OnStep;
         rightSide.Add(appplyStep);
 
+        Box playBar = new Box(Orientation.Horizontal, 5);
+        Button play = new Button();
+        play.Clicked += OnPlay;
+        play.Image = new Image("icons/play.png");
+        playBar.Add(play);
+        Button pause = new Button();
+        pause.Clicked += OnPause;
+        pause.Image = new Image("icons/pause.png");
+        playBar.Add(pause);
+        rightSide.Add(playBar);
+        
+
         rightSide.Margin = 5;
         mainBox.PackStart(mainBoard, true, true, 0);
         mainBox.Add(rightSide);
         Add(mainBox);
-        Timeout.Add(500, OnTimeout);
+        Timeout.Add(100, OnTimeout);
     }
 
     protected override bool OnDeleteEvent(Event e) {
@@ -242,6 +274,14 @@ class BoardWindow : Gtk.Window {
         }
         return true;
     }
+
+    void OnPlay(object? sender, EventArgs e){
+        playing = true;
+    }
+
+    void OnPause(object? sender, EventArgs e){
+        playing = false;
+    }
 }
 
 // Main execution loop of the program
@@ -249,14 +289,13 @@ class MainLoop {
     public static void Main(string[] args){
         Application.Init();
 
-        SimBoard board = new SimBoard(20);
+        SimBoard board = new SimBoard(50);
         BoardWindow main = new BoardWindow(board);
         main.Draw(board);
-        // ControlWindow control = new ControlWindow();
-
-        // control.ShowAll();
+        
         main.ShowAll();
         Application.Run();
     }
 
 }
+
